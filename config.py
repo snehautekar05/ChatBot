67921dc9331d9de8e16cf7a5cb3826bd50a6f104
2021.sneha.utekar@ves.ac.in <2021.sneha.utekar@ves.ac.in>
2024-01-22 11:32:32 +0530

    modified

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config(object):
+    DEBUG = True
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    SECRET_KEY = "this-is-a-super-secret-key"
+    OPENAI_KEY = "your secret key"
+    print(OPENAI_KEY)
+config = {
+    'development': DevelopmentConfig,
+    'testing': DevelopmentConfig,
+    'production': DevelopmentConfig
+}
